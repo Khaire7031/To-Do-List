91798a2700cbe7a211b62b5eb3459d84ea457212
Pranav Khaire <pranavkhaire28@gmail.com>
2023-12-20 13:11:48 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,43 @@
+const inputBox = document.getElementById("inputBox");
+
+const list_container = document.getElementById("list-container");
+
+function addtask(){
+    if(inputBox.value === ''){
+        alert("You Must write Something");
+    }else{
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list_container.appendChild(li);
+
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+
+    inputBox.value = "";
+    saveData()
+}
+
+list_container.addEventListener("click",function(e){
+    if(e.target.tagName === "LI"){
+        e.target.classList.toggle("checked");
+        saveData()
+    }else if(e.target.tagName === "SPAN"){
+        e.target.parentElement.remove();
+        saveData()
+    }
+},false);
+
+
+
+function saveData(){
+    localStorage.setItem("data",list_container.innerHTML);
+}
+
+
+function showTask(){
+    list_container.innerHTML = localStorage.getItem("data");
+}
+
+showTask()
